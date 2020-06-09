29330ba4f00a35c983c54b20d69b23551cbf4421
mihirane <mihiraneofficial@gmail.com>
2020-06-09 18:26:30 +0530

    final

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,13 @@
       How Coding Works
     </h1>
     <p>How Coding Works is a blogging website about technology and computer programming.</p>
+    <br><br><br>
+    <nuxt-link to="/AngularJs">
+      Angular Js
+    </nuxt-link>
+    <nuxt-link to="/Firebase">
+      Firebase
+    </nuxt-link>
   </div>
 </template>
 
